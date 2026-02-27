eef59a942f616a2144d704e2efbd959f11aa86e0
jacobdt23 <jacob.turner@gmail.com>
2026-02-26 23:36:58 -0500

    Sync: 2026-02-26 23:36:58

--- home.nix
+++ home.nix
@@ -30,39 +30,137 @@
     nerd-fonts.symbols-only 
   ];
 
-  # Silences Neovide and creates the Titus Fetch config
   home.file = {
+    # Silence Neovide noise
     ".config/neovide/config.toml".text = "fork = true";
     
+    # Chris Titus Custom Fastfetch Config
     ".config/fastfetch/config.jsonc".text = ''
       {
         "$schema": "https://github.com/fastfetch-cli/fastfetch/raw/dev/doc/json_schema.json",
         "logo": {
-            "type": "small",
-            "padding": { "top": 2, "left": 2 }
+          "padding": {
+            "top": 1
+          }
         },
         "display": {
-            "separator": " ➜  ",
-            "color": "magenta"
+          "separator": " 󰑃  "
         },
         "modules": [
-            "title",
-            "separator",
-            "os",
-            "host",
-            "kernel",
-            "uptime",
-            "packages",
-            "shell",
-            "display",
-            "de",
-            "wm",
-            "terminal",
-            "cpu",
-            "gpu",
-            "memory",
-            "break",
-            "colors"
+          "break",
+          {
+            "type": "os",
+            "key": " DISTRO",
+            "keyColor": "yellow"
+          },
+          {
+            "type": "kernel",
+            "key": "│ ├",
+            "keyColor": "yellow"
+          },
+          {
+            "type": "packages",
+            "key": "│ ├󰏖",
+            "keyColor": "yellow"
+          },
+          {
+            "type": "command",
+            "key": "│ ├",
+            "keyColor": "yellow",
+            "text": "birth_install=$(stat -c %W /); current=$(date +%s); time_progression=$((current - birth_install)); days_difference=$((time_progression / 86400)); echo $days_difference days"
+          },
+          {
+            "type": "shell",
+            "key": "│ └",
+            "keyColor": "yellow"
+          },
+          {
+            "type": "wm",
+            "key": " DE/WM",
+            "keyColor": "blue"
+          },
+          {
+            "type": "wmtheme",
+            "key": "│ ├󰉼",
+            "keyColor": "blue"
+          },
+          {
+            "type": "icons",
+            "key": "│ ├󰀻",
+            "keyColor": "blue"
+          },
+          {
+            "type": "cursor",
+            "key": "│ ├",
+            "keyColor": "blue"
+          },
+          {
+            "type": "terminalfont",
+            "key": "│ ├",
+            "keyColor": "blue"
+          },
+          {
+            "type": "terminal",
+            "key": "│ └",
+            "keyColor": "blue"
+          },
+          {
+            "type": "host",
+            "key": "󰌢 SYSTEM",
+            "keyColor": "green"
+          },
+          {
+            "type": "cpu",
+            "key": "│ ├󰻠",
+            "keyColor": "green"
+          },
+          {
+            "type": "gpu",
+            "key": "│ ├󰻑",
+            "keyColor": "green"
+          },
+          {
+            "type": "display",
+            "key": "│ ├󰍹",
+            "keyColor": "green",
+            "compactType": "original-with-refresh-rate"
+          },
+          {
+            "type": "memory",
+            "key": "│ ├󰾆",
+            "keyColor": "green"
+          },
+          {
+            "type": "swap",
+            "key": "│ ├󰓡",
+            "keyColor": "green"
+          },
+          {
+            "type": "uptime",
+            "key": "│ ├󰅐",
+            "keyColor": "green"
+          },
+          {
+            "type": "sound",
+            "key": " AUDIO",
+            "format": "{2}",
+            "keyColor": "magenta"
+          },
+          {
+            "type": "player",
+            "key": "│ ├󰥠",
+            "keyColor": "magenta"
+          },
+          {
+            "type": "media",
+            "key": "│ └󰝚",
+            "keyColor": "magenta"
+          },
+          {
+            "type": "custom",
+            "format": "\u001b[90m  \u001b[31m  \u001b[32m  \u001b[33m  \u001b[34m  \u001b[35m  \u001b[36m  \u001b[37m  \u001b[38m  \u001b[39m  \u001b[39m    \u001b[38m  \u001b[37m  \u001b[36m  \u001b[35m  \u001b[34m  \u001b[33m  \u001b[32m  \u001b[31m  \u001b[90m "
+          },
+          "break"
         ]
       }
     '';
@@ -70,16 +168,13 @@
 
   programs.bash = {
     enable = true;
-    # Auto-run fetch on terminal start
     initExtra = "fastfetch";
-    
     shellAliases = {
       rebuild = "sudo nixos-rebuild switch --flake ~/nixos-config#nixos";
       gsync = "git add . && git commit -m \"Sync: $(date +'%Y-%m-%d %H:%M:%S')\" && git push";      
       cleanup = "sudo nix-collect-garbage --delete-older-than 7d";
       listgens = "sudo nix-env --list-generations --profile /nix/var/nix/profiles/system";
       
-      # Corrected Silent GUI Aliases
       editconf = "neovide ~/nixos-config/configuration.nix > /dev/null 2>&1 & disown";
       edithome = "neovide ~/nixos-config/home.nix > /dev/null 2>&1 & disown";
       editapps = "neovide ~/nixos-config/system-apps.nix > /dev/null 2>&1 & disown";
