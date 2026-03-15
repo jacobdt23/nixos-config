39f7fa1c3011263150dda3c3d9272766d3dd3dff
jacobdt23 <jacob.turner@gmail.com>
2026-03-15 15:32:17 -0400

    Gen 103 (2026-03-15 ) Rebuild:added fastfetch

--- home.nix
+++ home.nix
@@ -33,28 +33,149 @@ in
     drs-fix
     symbola
     nerd-fonts.symbols-only
+    fastfetch
+    tree
   ];
 
-  home.file.".config/fastfetch/config.jsonc".text = ''
-    {
-      "modules": [
-        "title",
-        "separator",
-        "os",
-        "kernel",
-        "uptime",
-        "packages",
-        "shell",
-        "de",
-        "wm",
-        "cpu",
-        "gpu",
-        "memory",
-        "break",
-        "colors"
-      ]
-    }
-  '';
+  # --- Managed Configuration Files ---
+  home.file = {
+    ".config/neovide/config.toml".text = "fork = true";
+
+    # Professional Fastfetch Layout
+    ".config/fastfetch/config.jsonc" = {
+      force = true;
+      text = ''
+        {
+          "$schema": "https://github.com/fastfetch-cli/fastfetch/raw/dev/doc/json_schema.json",
+          "logo": {
+            "padding": {
+              "top": 1
+            }
+          },
+          "display": {
+            "separator": " 󰑃  "
+          },
+          "modules": [
+            "break",
+            {
+              "type": "os",
+              "key": " DISTRO",
+              "keyColor": "yellow"
+            },
+            {
+              "type": "kernel",
+              "key": "│ ├",
+              "keyColor": "yellow"
+            },
+            {
+              "type": "packages",
+              "key": "│ ├󰏖",
+              "keyColor": "yellow"
+            },
+            {
+              "type": "command",
+              "key": "│ ├",
+              "keyColor": "yellow",
+              "text": "birth_install=$(stat -c %W /); current=$(date +%s); time_progression=$((current - birth_install)); days_difference=$((time_progression / 86400)); echo $days_difference days"
+            },
+            {
+              "type": "shell",
+              "key": "│ └",
+              "keyColor": "yellow"
+            },
+            {
+              "type": "wm",
+              "key": " DE/WM",
+              "keyColor": "blue"
+            },
+            {
+              "type": "wmtheme",
+              "key": "│ ├󰉼",
+              "keyColor": "blue"
+            },
+            {
+              "type": "icons",
+              "key": "│ ├󰀻",
+              "keyColor": "blue"
+            },
+            {
+              "type": "cursor",
+              "key": "│ ├",
+              "keyColor": "blue"
+            },
+            {
+              "type": "terminalfont",
+              "key": "│ ├",
+              "keyColor": "blue"
+            },
+            {
+              "type": "terminal",
+              "key": "│ └",
+              "keyColor": "blue"
+            },
+            {
+              "type": "host",
+              "key": "󰌢 SYSTEM",
+              "keyColor": "green"
+            },
+            {
+              "type": "cpu",
+              "key": "│ ├󰻠",
+              "keyColor": "green"
+            },
+            {
+              "type": "gpu",
+              "key": "│ ├󰻑",
+              "format": "{2}",
+              "keyColor": "green"
+            },
+            {
+              "type": "display",
+              "key": "│ ├󰍹",
+              "keyColor": "green",
+              "compactType": "original-with-refresh-rate"
+            },
+            {
+              "type": "memory",
+              "key": "│ ├󰾆",
+              "keyColor": "green"
+            },
+            {
+              "type": "swap",
+              "key": "│ ├󰓡",
+              "keyColor": "green"
+            },
+            {
+              "type": "uptime",
+              "key": "│ ├󰅐",
+              "keyColor": "green"
+            },
+            {
+              "type": "sound",
+              "key": " AUDIO",
+              "format": "{2}",
+              "keyColor": "magenta"
+            },
+            {
+              "type": "player",
+              "key": "│ ├󰥠",
+              "keyColor": "magenta"
+            },
+            {
+              "type": "media",
+              "key": "│ └󰝚",
+              "keyColor": "magenta"
+            },
+            {
+              "type": "custom",
+              "format": "\u001b[90m  \u001b[31m  \u001b[32m  \u001b[33m  \u001b[34m  \u001b[35m  \u001b[36m  \u001b[37m  \u001b[38m  \u001b[39m  \u001b[39m    \u001b[38m  \u001b[37m  \u001b[36m  \u001b[35m  \u001b[34m  \u001b[33m  \u001b[32m  \u001b[31m  \u001b[90m "
+            },
+            "break"
+          ]
+        }
+      '';
+    };
+  };
 
   programs.bash = {
     enable = true;
@@ -86,7 +207,7 @@ in
           return 1
         fi
       }
-      showcase
+      fastfetch
     '';
 
     shellAliases = {
@@ -97,6 +218,7 @@ in
       editconf = "neovide ~/nixos-config/configuration.nix > /dev/null 2>&1 & disown";
       edithome = "neovide ~/nixos-config/home.nix > /dev/null 2>&1 & disown";
       editapps = "neovide ~/nixos-config/system-apps.nix > /dev/null 2>&1 & disown";
+      health = "nvidia-smi && echo '' && zramctl";
       doom = "/home/jacob/.config/emacs/bin/doom";
       l = "ls -alh";
       ll = "ls -l";
