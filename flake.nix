48044eddb017cf1bc206936bd4ac3dfa395971b3
jacobdt23 <turnejac01@gmail.com>
2026-03-24 01:14:35 -0400

    Rebuild (vm): Tue Mar 24 01:14:35 AM EDT 2026

--- flake.nix
+++ flake.nix
@@ -5,28 +5,27 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
-    nix-cachyos-kernel.url = "github:googleson78/nix-cachyos-kernel";
+    # UPDATED URL HERE:
+    nix-cachyos-kernel.url = "github:CachyOS/nix-cachyos-kernel";
   };
 
   outputs = { self, nixpkgs, home-manager, ... }@inputs: {
     nixosConfigurations = {
-      # --- BARE METAL HOST (RTX 5070 Rig) ---
       nixos = nixpkgs.lib.nixosSystem {
         specialArgs = { inherit inputs; };
         modules = [
           ./configuration.nix
-          ./hardware-configuration.nix # Real NVMe/SSD
-          ./nvidia.nix                # Real GPU Drivers
+          ./hardware-configuration.nix
+          ./nvidia.nix
           home-manager.nixosModules.home-manager
         ];
       };
 
-      # --- VIRTUAL MACHINE (The Lab) ---
       vm = nixpkgs.lib.nixosSystem {
         specialArgs = { inherit inputs; };
         modules = [
           ./configuration.nix
-          ./vm-hardware.nix           # Virtual Drive Only
+          ./vm-hardware.nix
           home-manager.nixosModules.home-manager
           {
             networking.hostName = "vm";
@@ -36,4 +35,3 @@
     };
   };
 }
-
