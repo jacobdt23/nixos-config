ac2861160416724edb3c54ed585518f5bb9c2b95
jacobdt23 <jacob.turner@gmail.com>
2026-03-28 23:25:54 -0400

    Rebuild (nixos)

--- system-apps.nix
+++ system-apps.nix
@@ -11,7 +11,7 @@ in
 
   environment.systemPackages = with pkgs; [
      (if lib.elem "nvidia" config.services.xserver.videoDrivers then nvtopPackages.full else nvtopPackages.amd)
-   
+    gimp
     github-cli 
     hugo
     virt-viewer
